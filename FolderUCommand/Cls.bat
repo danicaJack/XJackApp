8a6297de3baa5898f1a7f2afb9869e75a5f56430
danicaJack <dual.jackson@yandex.com>
2014-02-05 15:53:19 +0200

    Update Cls.bat

--- FolderUCommand/Cls.bat
+++ FolderUCommand/Cls.bat
@@ -1,17 +1,17 @@
 @echo off
 
 set delOtherPath=C:\Program Files (x86)\Microsoft Office\Office12\LAUNCHES\Service\OFiles
-FOR /f "tokens=*" %%a in ('dir /b "%delOtherPath%" ^| findstr /i /C:"26.12.2013"') do (
+FOR /f "tokens=*" %%a in ('dir /b "%delOtherPath%" ^| findstr /i /C:"27.01.2014" /C:"26.12.2013"') do (
 del /F /Q "%delOtherPath%\%%a"
 )
 
 set delScrPath=C:\Program Files (x86)\Microsoft Office\Office12\LAUNCHES\Service\OFiles\ScrFiles
-FOR /f "tokens=*" %%b in ('dir /b "%delScrPath%" ^| findstr /i /C:"26.12.2013"') do (
+FOR /f "tokens=*" %%b in ('dir /b "%delScrPath%" ^| findstr /i /C:"27.01.2014" /C:"26.12.2013"') do (
 del /F /Q "%delScrPath%\%%b"
 )
 
 set delWordPath=C:\Program Files (x86)\Microsoft Office\Office12\LAUNCHES\Service\WFiles
-FOR /f "tokens=*" %%c in ('dir /b "%delWordPath%" ^| findstr /i /C:"26.12.2013"') do (
+FOR /f "tokens=*" %%c in ('dir /b "%delWordPath%" ^| findstr /i /C:"27.01.2014" /C:"26.12.2013"') do (
 del /F /Q "%delWordPath%\%%c"
 )
 
